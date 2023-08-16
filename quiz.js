8af34e9c55d4ba418e9d6841706b44446aa32d6a
MAT3U55 <123771977+MAT3U55@users.noreply.github.com>
2023-08-16 16:48:25 -0300

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,15 @@
+const resultado = document.getElementById("resultado");
+
+function verificarResposta(resposta, proximaPergunta) {
+    if (resposta === "b") {
+        resultado.innerHTML = "Resposta correta     👍";
+        resultado.style.color = "green";
+
+        setTimeout(function() {
+            window.location.href = proximaPergunta;
+        }, 1900);
+    } else {
+        resultado.innerHTML = "Resposta incorreta👎";
+        resultado.style.color = "red";
+    }
+}
